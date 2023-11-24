964c1fade50111451c9ee79c249653a91cc33ace
karamelkha <khalturinaks@gmail.com>
2023-11-24 13:27:52 +0300

    context

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
 import  styles from "./Header.module.css"
 
-function Header() {
+function Header({changedUser}) {
+
+  const changeUser = (e)=>{
+    console.log(e.target.value)
+  } 
 
     return (
       <>
       <h1 className={styles.header}>Мои заметки</h1>
+      <select name="user" id="user" onChange={changeUser}>
+        <option value="1">Ксю</option>
+        <option value="2">Кир</option>
+      </select>
       </>
     )
 }
